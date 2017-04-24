af4d345d87d61862387ac1be74030f2cdc0553af
Jack Wearden <NotBobTheBuilder@users.noreply.github.com>
2017-04-24 11:27:43 -0700

    ssl=who?

--- db-connections.js
+++ db-connections.js
@@ -3,8 +3,8 @@ var MongoClient = require('mongodb').MongoClient;
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-var PROD_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>&ssl=true";
-var MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>&ssl=true";
+var PROD_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>";
+var MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>";
 
 var databases = {
   production: async.apply(MongoClient.connect, PROD_URI),
